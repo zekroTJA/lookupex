dbb98d2d5ffa40ef9aa34823aafdec17ce63461c
Ringo Hoffmann <riho@live.de>
2021-05-20 13:58:35 +0200

    fix bind address variable

--- lib/application.ex
+++ lib/application.ex
@@ -1,10 +1,11 @@
 defmodule Lookupex.Application do
   use Application
+  require Logger
 
   def start(_type, _args) do
     Envy.auto_load()
 
-    bind_split = System.get_env("BIND_ADDR", "0.0.0.0") |> String.split(":")
+    bind_split = System.get_env("BIND_ADDRESS", "0.0.0.0") |> String.split(":")
     bind_split = if length(bind_split) < 2, do: bind_split ++ ["8080"], else: bind_split
 
     ip =
@@ -30,12 +31,11 @@ defmodule Lookupex.Application do
       Lookupex.Cache
     ]
 
+    Logger.info("Webserver running at addr #{bind_split |> Enum.join(":")}")
+
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Lookupex.Supervisor]
     Supervisor.start_link(children, opts)
-
-    # Lookupex.Discord.start_link()
-    # Lookupex.Cache.start_link()
   end
 end
